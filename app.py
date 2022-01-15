41a65480017bfcb31fdf424c0367c719d386cd75
341970 <vitalystriz@ya.ru>
2022-01-15 19:24:48 +0300

    six

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 import sqlite3
 app=Flask(__name__)
 
+if __name__=="__main__":
+    app.run()
+
 def get_db_connection():
     conn=sqlite3.connect("database.db")
     conn.row_factory=sqlite3.Row
@@ -15,17 +18,10 @@ def draw_main_page():
     conn.close()
 
     return render_template("index.html",posts=posts)
-@app.route("/<int:post_id>")
-def post(post_id):
-    conn=get_db_connection()
-    post= conn.execute("SELECT * from posts WHERE" )
+# @app.route("/<int:post_id>")
+# def post(post_id):
+#     conn=get_db_connection()
+#     post= conn.execute("SELECT * from posts". )
 
-@app.route("/about")
-def description():
-
-
-
-
-
-if __name__=="__main__":
-    app.run()
+# @app.route("/about")
+# def description():
